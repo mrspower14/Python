46d4b9d5c76d36fa5ab695b3af333e46b0302bb3
SHIM HEE KYOUNG <mrspower14@gmail.com>
2026-04-16 11:13:53 +0900

    python test

---
+++ 01_data.py
@@ -0,0 +1,27 @@
+print(type(10));
+
+print(4+10);
+print(20*20)
+print(1/2)       
+print(11//2);    
+print(4**2);
+
+num=4;
+print(num);
+num+=4;
+print(num);
+num-=3;
+print(num);
+num**=2;
+print(num);
+
+print(5 < 6);
+print(13 == 13);
+print( 11 != 5);
+print( 13 == 4);
+
+hello = 'Hello '
+hello += 'World!'
+print(hello)
+hello *= 3
+print(hello)
